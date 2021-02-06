56b9570d85d419aa6481c04d7281e28883ce4b5f
Sam Miller <samuelsmiller@gmail.com>
2021-02-06 12:16:20 -0500

    Proxy rearranging.

--- Alyssa/messageAPI.py
+++ Alyssa/messageAPI.py
@@ -10,14 +10,70 @@ from Collections import defaultdict
 class Proxy:
     def __init__(self):
         self.context = zmq.Context()
-        self.topic_publishers = defaultdict(list)
-        self.topic_subscribers = defaultdict(list)
+        #self.topic_publishers = defaultdict(list)
+        #self.topic_subscribers = defaultdict(list)
+        self.xsubsocket = None
+        self.xpubsocket = None
+        self.poller = zmq.Poller()
 
     def get_context(self): 
         return self.context 
 
-    def notify(self, topic, value):
-        pass
+    def create_XSub(self):
+        self.xsubsocket = get_context.socket(zmq.XSUB)
+        self.xsubsocket.bind("tcp://*:5555")
+        self.register_poll(xsubsocket)
+        return self.xsubsocket
+
+    def create_XPub(self):
+        self.xpubsocket = get_context.socket (zmq.XPUB)
+        self.xpubsocket.setsockopt(zmq.XPUB_VERBOSE, 1)
+        self.xpubsocket.bind ("tcp://*:5556")
+        self.register_poller(xpubsocket)
+        return self.xpubsocket
+
+    # unsure of parameters
+    def register_poller(entity_id):
+        self.poller.register(entity_id, zmq.POLLIN)
+        return self.poller
+
+    def poll():
+        print ("Poll with a timeout of 1 sec")
+        self.events = dict (poller.poll (1000))
+        print ("Events received = {}".format (events))
+        getPubData()
+        getSubData()
+
+    def getPubData(self):
+        # Is there any data from publisher? Note that the proxy
+        # will get only that data for which it has relayed
+        # subscriptions to the publisher. Note that ZMQ in recent
+        # versions is doing filtering on the publisher side. Thus,
+        # not all data from publishers will even show up on proxy.
+        if self.xsubsocket in self.events:
+            msg = self.xsubsocket.recv_multipart()
+            print ("Publication = {}".format (msg))
+            # send the message to subscribers
+            self.xpubsocket.send_multipart (msg)
+
+
+    def getSubData(self):
+    # Is there any data from subscriber? Note, this is
+        # needed because we must relay the subscription info
+        # to all my publishers else they will never send anything
+        # to us given that we are a XSUB/XPUB entity
+        if xpubsocket in events:
+            msg = xpubsocket.recv_multipart()
+            print("Subscription = {}".format(msg))
+            # send the subscription info to publishers
+            xsubsocket.send_multipart(msg)
+
+    def run(self):
+        while True:
+            try:
+                self.poll()
+            except:
+                print("Exception thrown: {}".format(sys.exc_info()[0]))
 
         #what to do with topic in register_pub?
         #I think we store the topic into a dictionary with the IP.
@@ -87,77 +143,14 @@ class Subscriber:
     def notify(topic_filter, value):
         pass 
 
-#how to process and print message in sub
-def process_msg():
-    # Process 5 updates
-    total_temp = 0
-    for update_nbr in range(5):
-        string = socket.recv_multipart()
-        # zipcode, temperature, relhumidity = string.split()
-        # total_temp += int(temperature)
+    #how to process and print message in sub
+    def process_msg():
+                                   # Process 5 updates
+        total_temp = 0
+        for update_nbr in range(5):
+                string = self.socket.recv_multipart()
+                zipcode, temperature, relhumidity = string.split(" ")
+                total_temp += int(temperature)
 
         print("Average temperature for zipcode '%s' was %dF" % (
             topic_filter, total_temp / (update_nbr+1))
-
-xsubsocket;
-xpubsocket;
-poller; 
-
-
-def create_XSub():
-    xsubsocket = get_context.socket(zmq.XSUB)
-    xsubsocket.bind("tcp://*:5555")
-    return xsubsocket
-
-
-def create_XPub():
-    xpubsocket = get_context.socket (zmq.XPUB)
-    xpubsocket.setsockopt(zmq.XPUB_VERBOSE, 1)
-    xpubsocket.bind ("tcp://*:5556")
-    return xpubsocket
-
-# unsure of parameters
-def create_poller(sub = <xsubsocket>, pub = <xpubsocket>):
-    poller = zmq.Poller ()
-    poller.register (sub, zmq.POLLIN)
-    poller.register (pub, zmq.POLLIN)
-    return poller
-
-def poll():
-    print ("Poll with a timeout of 1 sec")
-
-    events = dict (poller.poll (1000))
-    print ("Events received = {}".format (events))
-
-def getPubData():
-        # Is there any data from publisher? Note that the proxy
-        # will get only that data for which it has relayed
-        # subscriptions to the publisher. Note that ZMQ in recent
-        # versions is doing filtering on the publisher side. Thus,
-        # not all data from publishers will even show up on proxy.
-        if xsubsocket in events:
-            msg = xsubsocket.recv_multipart()
-
-            print ("Publication = {}".format (msg))
-
-            # send the message to subscribers
-            xpubsocket.send_multipart (msg)
-
-
-
-
-
-
-def sendSubData():
-    # Is there any data from subscriber? Note, this is
-        # needed because we must relay the subscription info
-        # to all my publishers else they will never send anything
-        # to us given that we are a XSUB/XPUB entity
-        if xpubsocket in events:
-            msg = xpubsocket.recv_multipart()
-
-            # parse the incoming message
-            print ("subscription = {}".format (msg))
-
-            # send the subscription info to publishers
-            xsubsocket.send_multipart(msg)
